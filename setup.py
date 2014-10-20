03e01df6c42a855fde95917ee8a92350e477edbb
Dan Foster <dan.foster@bristol.ac.uk>
2014-10-20 14:36:59 +0100

    Add RPM deps

--- setup.py
+++ setup.py
@@ -2,6 +2,19 @@
 
 from setuptools import setup, find_packages
 
+RPM_REQUIRED_DEPS = "python-argparse PyYAML python-dateutil python-json"
+ 
+## HACK FOR DEPS IN RPMS
+from setuptools.command.bdist_rpm import bdist_rpm
+def custom_make_spec_file(self):
+    spec = self._original_make_spec_file()
+    lineDescription = "%description"
+    spec.insert(spec.index(lineDescription) - 1, "requires: %s" % RPM_REQUIRED_DEPS)
+    return spec
+bdist_rpm._original_make_spec_file = bdist_rpm._make_spec_file
+bdist_rpm._make_spec_file = custom_make_spec_file
+## END OF HACK
+
 setup(
     name = "mmbackupagent",
     version = "0.1",
